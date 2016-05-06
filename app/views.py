5d600e988bb5cd1decf80657098abeb50d93b9b2
jkamashian <jacobkamashian@gmail.com>
2016-05-05 18:53:59 -0600

    working version meets all requirements

--- app/views.py
+++ app/views.py
@@ -1,47 +1,72 @@
 from app import app
 from datetime import datetime
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
 from flask import request
-from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults
+from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, ZillowError
 from pymongo import MongoClient
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+VALID_ZAZ_ERR = "please post with valid 'zwsid' 'address' 'zipcode' "
+VALID_R_ID_ERR = "please post with valid 'r_id'"
+VALID_ZWSID_ERR = "Missing 'zwsid'"
+VALID_ADDRESS_ERR = "Missing 'address'"
+VALID_ZIPCODE_ERR = "missing 'zipcode'"
 
 @app.route('/zillow_data', methods=('GET', 'POST'))
 def get_data():
+    """
+    Returns zestimate_amount with a valid r_id
+    if a user enters an invalid r_id or dosen't post
+    return VALID_R_ID_ERR error message
+    """
     if request.method == 'POST':
         if request.values.get('r_id'):
-            r_id = request.values.get('r_id')
-            client = MongoClient()
-            db = client.zillow
-            res = db.find_one({"_id": r_id})
-            return 'working', r_id
+            try:
+                r_id = request.values.get('r_id')
+                client = MongoClient()
+                db = client.zillow
+                coll = db.zillow
+                res = coll.find_one({"_id": ObjectId(r_id)})
 
-    return "please post with valid 'r_id"
+                print 'r_id:', r_id
+                if res:
+                    return res['zillow_responce'].get('zestimate_amount')
+            except InvalidId:
+                pass
+
+    return "please post with valid 'r_id'"
 
 
+@app.route('/')
 @app.route('/zillow', methods=('GET', 'POST'))
 def zillow():
+    """
+    returns r_id with a valid search
+    returns VALID_ZAZ_ERR with invalid search
+    returns VALID_ZWSID_ERR with missing zwsid
+    returns VALID_ADDRESS_ERR with missing address
+    returns VALID_ZIPCODE_ERR with missing zipcode
+    """
     if request.method == 'POST':
         if request.values.get('zwsid'):
             zwsid = request.values.get('zwsid')
         else:
-            return "Missing zwsid"
+            return VALID_ZWSID_ERR
         if request.values.get('address'):
             address = request.values.get('address')
         else:
-            return "Missing address"
+            return VALID_ADDRESS_ERR
         if request.values.get('zipcode'):
             zipcode = request.values.get('zipcode')
         else:
-            return "missing zipcode"
-        zillow_data = ZillowWrapper(zwsid)
-        deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
-        result = GetDeepSearchResults(deep_search_response)
+            return VALID_ZIPCODE_ERR
+        try:
+            zillow_data = ZillowWrapper(zwsid)
+            deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
+            result = GetDeepSearchResults(deep_search_response)
+        except ZillowError:
+            return VALID_ZAZ_ERR
 
         client = MongoClient()
         db = client.zillow
@@ -81,4 +106,4 @@ def zillow():
             }
         )
         return str(result.inserted_id)
-    return "please post with valid 'zwsid' 'address' 'zipcode' "
+    return VALID_ZAZ_ERR
